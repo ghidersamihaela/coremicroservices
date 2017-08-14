06e342e791ebc3367fe45823f74904079891e68b
ghidersamihaela <ghidersa.mihaela@gmail.com>
2017-08-14 20:51:38 +0300

    test2

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ShoppingCart
     public class Program
     {
         public static void Main(string[] args)
-        { 
+        {  
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
